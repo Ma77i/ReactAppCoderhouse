9282f702da560b27a2ca0bcebb5bd6663a349362
Ma77i <mattiasuru@gmail.com>
2021-07-23 14:41:01 -0300

    desafio 2 crear Header y NavBar

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import NavBar from "./NavBar"
+
+const Header = () => 
+    <header>
+        <h1>Tienda</h1> 
+        <NavBar />
+    </header>
+
+    export default Header
---
+++ src/NavBar.js
@@ -0,0 +1,8 @@
+const NavBar = () =>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Shop</a>
+        <a href="#">Contact</a>
+    </nav>
+
+    export default NavBar
