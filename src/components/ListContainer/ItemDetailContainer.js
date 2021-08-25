a5ab49748bce4a77a2569f7c809aaf4a92feebe9
Ma77i <mattiasuru@gmail.com>
2021-08-25 15:47:15 -0300

    firebase en list y detail

--- src/components/ListContainer/ItemDetailContainer.js
+++ src/components/ListContainer/ItemDetailContainer.js
@@ -2,17 +2,18 @@ import { useState, useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import ItemDetail from './ItemDetail'
-import { POKES } from "../../data"
-
+//import { POKES } from "../../data"
+import Loading from "../Loading";
+import {firestore} from '../../firebase'
 
 const ItemDetailContainer = () => {
 
     const [product, setProduct] = useState({})
-    
+    const [state, setState] = useState('pendiente')
     const detailParams = useParams()
     useEffect(() => {
 
-
+/*
         const getItems = () => {
             return new Promise((resolve, reject) => {
                 setTimeout(() => {
@@ -26,15 +27,50 @@ const ItemDetailContainer = () => {
 
         setProduct({});
         getItems().then((resolve) => setProduct(resolve));
+*/
 
-    }, [detailParams]);
+        const dataBase = firestore;
+        const collection = dataBase.collection('products');
+
+        if (detailParams.id) {
+
+            const filter = collection.doc(detailParams.id)
+            const query = filter.get()
+
+            query.then((results) => {
+                const id = results.id;
+
+                const data = results.data()
+                const data_final = {id, ...data}
+                setProduct(data_final)
+
+            }).finally(() => {
+                setState('Terminado')
+            })
+        }else{
+            setState('Terminado')
+        }
+
+        
+
+    }, [detailParams.id]);
+
+    if (state === 'pendiente') {
+        return (
+            <Container>
+            <h1>Detalles</h1>
+            <Loading />
+        </Container>
+        )
+    }
 
 
     return (
         <Container>
             <h1>Detalles</h1>
             <ItemDetail item={product} />
-        </Container>)
+        </Container>
+        )
 
 }
 
--- src/components/ListContainer/ItemListContainer.js
+++ src/components/ListContainer/ItemListContainer.js
@@ -3,7 +3,6 @@ import ItemList from "./ItemList"
 import { Container } from 'react-bootstrap'
 import { useState, useEffect } from "react"
 import { useParams } from "react-router";
-//import { POKES } from "../../data"
 import Loading from "../Loading";
 import {firestore} from '../../firebase'
 
@@ -11,70 +10,26 @@ const ItemListContainer = ({ greeting }) => {
 
 
     const [data, setData] = useState([])
-
-    //const [load, setLoad] = useState(true)
     const params = useParams()
-    //console.log(load);
 
     useEffect(() => {
-/*
-        const promise = new Promise((resolve, reject) => {
 
-            setLoad(true)
+        
+        const dataBase = firestore  
 
-            setTimeout(() => {
-                console.log("setTimeOut");
-                if (params.id) {
-                    resolve(POKES.filter(prod => prod.category === params.id))
-                } else {
-                    resolve(POKES)
-                };
-                reject("Ocurrió un error al traer los productos")
-
-            }, 2000)
-        })
-
-        promise.then((resolve) => {
-            console.log("Cargado con exito");
-            setData(resolve)
-        }).then(setLoad(false))
-        */
-/*
-        let collection = firestore().collection('products')
-
-        if (typeof id !== 'undefined') {
-            collection = collection.where('category', '==', id)
-        }
-
-        collection.limit(20)
-            .get()
-            .then(querySnapshot => {
-                setData(querySnapshot.docs.map(doc => {
-                    return {id: doc.id, ...doc.data()}
-                }))
-            }).catch(() => setData([]))
-
-*/
-    const dataBase = firestore  
-
-    const collection = dataBase.collection('products')
-
-    const query = collection.get()
+        const collection = dataBase.collection('products')
+        
+        const query = collection.get()
         query.then((result) => {
 
             const parseResult = []
             result.forEach((document)=>{
-                const id = document.id
-                const data = document.data()
-                const data_final = {id, ...data}
-
-                console.log(data_final);
-                parseResult.push(data_final)
+                parseResult.push({...document.data(), id: document.id})
             });
             setData(parseResult)
 
         }).catch((err) => {
-            console.log('Datos no encontrados');
+            console.log('Datos no encontrados', err);
         });
 
         
